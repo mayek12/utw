af5a82d78dcd4f52626ab79516c18aa22c461810
Mariusz Walkowicz <mariusz@debian.debian.pl>
2019-04-19 10:41:48 +0200

    Zamina nazwy2

--- resources/views/edit_art.blade.php
+++ resources/views/edit_art.blade.php
@@ -4,7 +4,7 @@
 
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Edycja artykułu</div>
+                <div class="card-header">Edycja/Aktualizacja artykułu</div>
 
               First Artykuł
 
